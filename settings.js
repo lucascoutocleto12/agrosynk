56ca361152aad03d6029ec2ef1ed551b67c4f576
Lucas Couto <99884588+lucascoutocleto12@users.noreply.github.com>
2026-06-16 00:28:34 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,18 +1,8 @@
 module.exports = {
     uiPort: process.env.PORT || 1880,
-
     flowFile: "flows.json",
-
     credentialSecret: process.env.CREDENTIAL_SECRET || "agrosynk-secret",
-
     httpAdminRoot: "/admin",
     httpNodeRoot: "/",
-
-    functionGlobalContext: {},
-
-    editorTheme: {
-        projects: {
-            enabled: false
-        }
-    }
+    functionGlobalContext: {}
 };
